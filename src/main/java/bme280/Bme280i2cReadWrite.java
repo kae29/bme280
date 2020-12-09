8fb4da0de0e4ae2fe535d4af3b29f3c4ad437403
kae29 <67366905+kae29@users.noreply.github.com>
2020-12-09 17:55:56 +0300

    add templates for basic read-write operations

--- src/main/java/bme280/Bme280i2cReadWrite.java
+++ src/main/java/bme280/Bme280i2cReadWrite.java
@@ -1,5 +1,37 @@
 package bme280;
 
+/**
+ * This class contain basic read-write i2c methods for BME 280.
+ */
 public class Bme280i2cReadWrite {
-    
+    /**
+     * i2c Device driver
+     */
+    private I2CDevice i2CDevice;
+
+    /**
+     * Default constructor.
+     * @param i2CDevice - i2c device driver
+     */
+    public Bme280i2cReadWrite(I2CDevice i2CDevice) {
+        this.i2CDevice = i2CDevice;
+    }
+
+    /**
+     * Read one byte from Register
+     * @param register BME 280 register
+     * @return register data
+     */
+    public int readRegister(int register) {
+        return 0;
+    }
+
+    /**
+     * Update register with one byte data.
+     * @param register BME 280 register
+     * @param data one byte data
+     */
+    public void writeRegister(int register, int data) {
+
+    }
 }
